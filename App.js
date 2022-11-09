ae33e1047f92296a67ef63d33aabaa157f8d80ff
Nguyen Hoan Huu <nguyenhoanhuu8668@gmail.com>
2022-11-09 21:01:28 +0700

    finish project

---
+++ App.js
@@ -0,0 +1,84 @@
+import { StatusBar } from 'expo-status-bar';
+import { useRef, useState  } from 'react';
+import { Animated, StyleSheet, Text, View } from 'react-native';
+import { Button, Image } from 'react-native';
+import faceCat from './assets/face_cat.jpg'
+
+
+export default function App() {
+
+  const [location, setLocation] 
+      = useState({
+        x: null,
+        y: null,
+        marginLeft: new Animated.Value(10),
+        marginTop: new Animated.Value(10)
+
+      });
+
+  function onPress(evt){
+       
+    
+    console.log('====================================');
+    var x = evt.nativeEvent.locationX;
+    console.log('====================================');
+    console.log(x);
+    console.log('====================================');
+    var y = evt.nativeEvent.locationY;
+    console.log('====================================');
+    setLocation({
+      x: x,
+      y: y,
+      marginLeft: x,
+      marginTop: y
+
+    })
+    
+    
+  }
+  function onMove(evt){
+    console.log('====================================');
+    console.log(location);
+    console.log('====================================');
+    //setLocation({marginLeft: x, marginTop: y })
+  }
+  function onRelease(){
+    console.log("Realse!");
+  }
+  const {marginTop, marginLeft} = location;
+  return (
+    <View
+      onStartShouldSetResponder={()=>true}
+      onMoveShouldSetResponder= {()=>true}
+      onResponderGrant={onPress}
+      onResponderMove={onMove}
+      onResponderRelease = {onRelease}
+      style={styles.container}>
+
+      <Text>Show something!</Text>
+      <Text>Show something!</Text>
+      <Animated.Image 
+       source = {faceCat}
+       style={{marginLeft: marginLeft, marginTop: marginTop,width:40,height:40}}
+       >
+      </Animated.Image>
+      <Animated.Image 
+       source = {faceCat}
+       style={{marginLeft: marginLeft, marginTop: marginTop,width:40,height:40}}
+       >
+      </Animated.Image>
+      <Animated.Image 
+       source = {faceCat}
+       style={{marginLeft: marginLeft, marginTop: marginTop,width:40,height:40}}
+       >
+      </Animated.Image>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+});
+
